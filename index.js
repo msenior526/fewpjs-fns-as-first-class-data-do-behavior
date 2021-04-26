3a2b2a7b2bb26af3429fb1adbf100363598546d6
msenior526 <msenior526@gmail.com>
2021-04-26 11:53:20 -0400

    commit tree 3d2d0263aa3453081acaf2f89de39c7a1cfa449e

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(militaryTime) {
+   let x = parseInt(militaryTime)
+   if (x < 12) {
+      return "Good Morning"
+   } 
+   else if (x > 17) {
+      return "Good Evening"
+   }
+   else {
+      return "Good Afternoon"
+   }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = msg
+}
